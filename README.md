f7063d44d118ebb0e830f1fb2fde56aafa588a60
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-05-17 07:26:06 +0200

    Notes on acks

--- README.md
+++ README.md
@@ -69,7 +69,6 @@ curl --request POST \
 ### Reseting the queues
 
 You can delete the queues running the script `reset_queues.js`:
-```sh
 
 The code wont work unless you comment the throw at the start of the file. This is done to avoid deleting the queues by mistake.
 
@@ -101,3 +100,67 @@ hostname: 'undefined'
   }
 
 docker run --rm -ti --env-file .env --network correctomatic-server_default correctomatic-server-starter /bin/sh
+
+
+
+## bullMQ information
+
+
+Redis keys:
+
+
+### `bull:<queueName>:meta` - hash
+Queue configuration?
+Keys I've seen:
+- opts.maxLenEvents
+
+### `bull:<queueName>:marker` - sorted set
+- ¿?
+
+### `bull:<queueName>:id` - string
+"autoincrement" id for the jobs
+
+### `bull:<queueName>:wait` - list
+ids of the jobs that are waiting to be processed (in reverse order?)
+
+### `bull:<queueName>:stalled` - list
+stalled jobs - jobs that have been in the active list for too long
+It seems that `worker.startStalledCheckTimer()` is the call that moves the jobs to this list
+
+### `bull:<queueName>:active` - list
+ids of active jobs
+When a job is being processed, it is moved from the wait list to the active list
+
+### `bull:<queueName>:completed` - list
+ids and timestamp of completed jobs
+
+### `bull:<queueName>:stalled-check` - string
+timestamp of the last time the stalled jobs were checked?
+
+### `bull:<queueName>:<job id>` - hash
+Information for a job in the queue.
+It's not deleted when the job is finished. How do we clean this?
+
+- name
+- data
+- opts
+- timestamp
+- delay
+- priority
+- processedOn (timestamp)
+- ats ¿?
+- atm?
+- returnvalue
+- finishedOn (timestamp)
+
+### `bull:<queueName>:<jobid>:lock` - string
+The token of the worker that is processing the job
+
+### `bull:<queueName>:events` - stream
+Events happening in the queue. Seen so far:
+- added
+- waiting
+- active: when a worker takes the job
+- completed
+- stalled
+- drained ¿?
--- src/bullmq_delayed_consumer.js
+++ src/bullmq_delayed_consumer.js
@@ -14,42 +14,59 @@ function currentTime() {
 
 const jobs = []
 
-function createPromise(){
-  let theResolve
-  const promise = new Promise(resolve => { theResolve = resolve})
-  promise.resolve = theResolve
-  return promise
+// Specify a unique token, shared between the worker and the async completer
+const token = 'banana!!!'
+
+const moreOptions = {
+  lockDuration: 30000,
+  /*
+  Duration of the lock for the job in milliseconds. The lock represents that a worker is processing the job.
+  If the lock is lost, the job will be eventually be picked up by the stalled checker and move back to wait
+  so that another worker can process it again.
+  */
+  maxStalledCount: 1,
 }
 
-const _worker = new Worker(QUEUE_NAME, async job => {
-  try {
-    console.log(`${currentTime()} - Processing job ${job.id}`)
+// Strategy to ack:
+// - Set a high lockDuration, enough to the docker containers have time to complete the correction
+//   (but not TOO high, or it won't be retried if the process crashes. Maybe 5 minutes is a good value?)
+// - Set a maxStalledCount to a high value, so the job is not aborted but retried.
+//   In theory the job shouldn't be retried a lot, perhaps one or two times if the docker container stalls, and two more
+//   if the completer process crashes. Maybe 5 is a good value?
+// - Renew locks somehow in the completer process? Don't seems very usefull, because if we renew indefinitely
+//   the job won't fail if the container stalls. I think setting a high lockDuration is enough.
 
-    const promise = createPromise()
-    const pendingJob = {id: job.id, promise: promise}
-
-    console.log('Adding job to pending list: ' + job.id)
-    jobs.push(pendingJob)
-    return await promise
-
-  } catch (error) {
-    console.error(error)
-  }
-}, {
+const worker = new Worker(QUEUE_NAME, null, {
   connection: redisConfig,
+  ...moreOptions
 })
 
+async function runWorker() {
+
+  await worker.startStalledCheckTimer()
+  const job = (await worker.getNextJob(token))
+  if(!job) {
+    console.log('Worker - No jobs available')
+    return
+  }
+  console.log(`Worker - Processing job ${job.id} - ${currentTime()}`)
+  jobs.push(job)
+  // await worker.close()
+}
+
+runWorker()
+setInterval(runWorker, 20000)
+
 setInterval(() => {
-  console.log('Checking for pending jobs')
-  const pendingJob = jobs.pop()
-  if (!pendingJob) return
+  console.log('Completer - Checking for pending jobs...')
+  const job = jobs.pop()
+  if (!job) return
 
-  const { id, promise } = pendingJob
+  // const { id, promise } = pendingJob
+  console.log(`Completer - Completing the job ${job.id} - ${currentTime()}`)
+  job.moveToCompleted('some return value', token, false)
 
-  console.log(`Completing the job ${id} outside of the worker `)
-  promise.resolve(currentTime())
-
-}, 5000)
+}, 15000)
 
 
 
