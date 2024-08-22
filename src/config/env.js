3aa4b5136e2472c2b65a44397a572d0e13f1c62e
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-08-22 08:29:58 +0200

    Changes options to boolean. Add option to automatically pull containers

--- src/config/env.js
+++ src/config/env.js
@@ -6,10 +6,36 @@ const DEFAULT_ENVIRONMENT = 'production'
 const DEFAULT_LOG_LEVEL = 'info'
 const DEFAULT_LOG_FILE = 'correctomatic.log'
 
+const DEFAULT_DOCKER_PULL='N'
+const DEFAULT_DONT_START_CONTAINER='N'
 const DEFAULT_DOCKER_TIMEOUT = 5000
+const DEFAULT_DOCKER_PULL_TIMEOUT = 15000
 const DEFAULT_DOCKER_OPTIONS = '{"socketPath": "/var/run/docker.sock"}'
 
 const DEFAULT_CONCURRENT_NOTIFIERS = 50
+
+// eslint-disable-next-line complexity
+function stringToBoolean(stringValue){
+  switch(stringValue?.toLowerCase()?.trim()){
+      case "true":
+      case "yes":
+      case "1":
+      case "y":
+        return true;
+
+      case "false":
+      case "no":
+      case "0":
+      case "n":
+      case "":
+      case null:
+      case undefined:
+        return false;
+
+      default:
+        return false;
+  }
+}
 
 export default {
   ENVIRONMENT: process.env.NODE_ENV || DEFAULT_ENVIRONMENT,
@@ -27,8 +53,10 @@ export default {
 
   docker: {
     DOCKER_TIMEOUT: Number(process.env.DOCKER_TIMEOUT || DEFAULT_DOCKER_TIMEOUT),
+    DOCKER_PULL: stringToBoolean(process.env.DOCKER_PULL || DEFAULT_DOCKER_PULL),
+    DOCKER_PULL_TIMEOUT: Number(process.env.DOCKER_PULL_TIMEOUT || DEFAULT_DOCKER_PULL_TIMEOUT),
     DOCKER_OPTIONS: process.env.DOCKER_OPTIONS || DEFAULT_DOCKER_OPTIONS,
-    DONT_START_CONTAINER: process.env.DONT_START_CONTAINER == 'S',
+    DONT_START_CONTAINER: stringToBoolean(process.env.DONT_START_CONTAINER || DEFAULT_DONT_START_CONTAINER),
   },
 
   bullmq: {
